990f73e4954eb64d810bd944cc11ad16ec462838
Brandon Meeks <brandon.meeks98@gmail.com>
2026-08-14 17:02:26 -0400

    schema successfully created and database seeded with randomly generated data, all test passing per TDD

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,19 @@
 -- TODO
+DROP TABLE IF EXISTS folders CASCADE;
+DROP TABLE IF EXISTS files;
+
+CREATE TABLE folders (
+    id SERIAL UNIQUE PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE files (
+    id serial PRIMARY KEY,
+    name TEXT NOT NULL,
+    size INT NOT NULL,
+    folder_id INT NOT NULL,
+    UNIQUE (name, folder_id),
+    FOREIGN KEY (folder_id)
+    REFERENCES folders(id)
+    ON DELETE CASCADE
+);
--- db/seed.js
+++ db/seed.js
@@ -5,6 +5,50 @@ await seed();
 await db.end();
 console.log("🌱 Database seeded.");
 
+function generateRandomString(length) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    // Pick a random index from the chars string
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    result += chars.charAt(randomIndex);
+  }
+  return result;
+}
+
 async function seed() {
-  // TODO
+  //gen 3 folders
+  for (let i = 1; i <= 3; i++) {
+    const folderSQL = `
+  INSERT INTO folders
+  (name)
+  VALUES ($1)
+  RETURNING *;
+  `;
+    try {
+      const res = await db.query(folderSQL, [generateRandomString(15)]);
+      //gen 5 files per folder
+      for (let j = 1; j <= 5; j++) {
+        const fileSQL = `
+  INSERT INTO files
+  (name, size, folder_id)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `;
+        try {
+          const res = await db.query(fileSQL, [
+            generateRandomString(15),
+            Math.round(Math.random() * 20),
+            i,
+          ]);
+          console.log(res.rows);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+      console.log(res.rows);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
